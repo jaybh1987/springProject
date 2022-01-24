ff3fcb2973b1f2079d776b8d2036040d983aeafa
laitmatus <laitmatus@pop-os.localdomain>
2022-01-24 16:02:30 +0530

    greeting controller is working.

--- src/main/java/com/example/springboot/controller/GreetingController.java
+++ src/main/java/com/example/springboot/controller/GreetingController.java
@@ -1,20 +1,17 @@
-package com.example.springboot;
+package com.example.springboot.controller;
 
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class GreetingController {
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name = "name", required = false, defaultValue="World")
-                           String name,
-                           Model model) {
-
-        model.addAttribute("name", name);
-        return "greeting";
+                           String name) {
+        System.out.println(name);
+        return "greeting ehllo ";
     }
 }
